51dd52bdab48bc7c4eb48487d6f4158f8acade7d
qiaw99 <872907968@qq.com>
2023-10-31 11:25:54 +0100

    remove redundant files

--- data/dataset_preprocessing.py
+++ data/dataset_preprocessing.py
@@ -7,6 +7,8 @@ labels = []
 evidences = []
 links = []
 
+label2int = {"REFUTED": 1, "SUPPORTED": 0}
+
 # Open the JSONL file
 with open("./COVIDFACT_dataset.jsonl", 'r') as file:
     for index, line in enumerate(file):
@@ -14,7 +16,7 @@ with open("./COVIDFACT_dataset.jsonl", 'r') as file:
             data = json.loads(line)
 
             claims.append(data["claim"])
-            labels.append(data["label"])
+            labels.append(label2int[data["label"]])
             links.append(data["gold_source"])
             evidence = data["evidence"]
 
@@ -27,20 +29,6 @@ with open("./COVIDFACT_dataset.jsonl", 'r') as file:
         except:
             print(line, index)
 
-
-name_dict = {
-    "claims": claims,
-    "labels": labels,
-    "evidences": evidences,
-    "links": links,
-  }
-
-df = pd.DataFrame(name_dict)
-df.to_csv('./COVIDFACT_dataset.csv', encoding='utf-8')
-
-
-### Label distribution visualization
-
 # REFUTED
 counter_0 = 0
 
@@ -48,13 +36,23 @@ counter_0 = 0
 counter_1 = 1
 
 for l in labels:
-  if l == "REFUTED":
-    counter_0 += 1
-  else:
-    counter_1 += 1
+    if l == 0:
+        counter_0 += 1
+    else:
+        counter_1 += 1
 
 text_labels = ["REFUTED", "SUPPORTED"]
 total = counter_0 + counter_1
 fig, ax = plt.subplots()
-size = [counter_0/total, counter_1/total]
+size = [counter_0 / total, counter_1 / total]
 ax.pie(size, labels=text_labels, autopct='%1.1f%%', shadow=True, startangle=90, colors=["lightcoral", "cornflowerblue"])
+
+name_dict = {
+    "claims": claims,
+    "labels": labels,
+    "evidences": evidences,
+    "links": links,
+}
+
+df = pd.DataFrame(name_dict)
+df.to_csv('./COVIDFACT_dataset.csv', encoding='utf-8')
