8d1cbb08528b1ffd4fb78a481007d686d2e5add7
qiaw99 <872907968@qq.com>
2024-01-08 12:44:39 +0100

    fix modelcard for GPTQ models

--- actions/metadata_utils.py
+++ actions/metadata_utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+MODEL_NAME = ["meta-llama/Llama-2-7b-chat-hf", "mistralai/Mistral-7B-v0.1,7B"]
+
 
 def get_metadata_by_model_name(model_name: str) -> str:
     """
@@ -7,10 +9,21 @@ def get_metadata_by_model_name(model_name: str) -> str:
     :param model_name: model name
     :return: metadata information
     """
+    if "Llama" in model_name:
+        model_name = MODEL_NAME[0]
+        gptq = True
+
+    if "Mistral" in model_name:
+        model_name = MODEL_NAME[1]
+        gptq = True
+
     df = pd.read_csv("./cache/metadata.csv")
     idx = df.index[df['model_name'] == model_name].tolist()
 
     return_s = f"<b>Model name:</b> {model_name}, <b>Model size: </b> {df['size'].loc[idx].values[0]}<br>"
+
+    if gptq:
+        return_s += "The model is quantized by GPTQ. "
     return_s += f"<b>Model description: </b> {df['description'].loc[idx].values[0]}<br>"
     return_s += f"<b>Training data: </b> {df['training_data'].loc[idx].values[0]}<br>"
 
